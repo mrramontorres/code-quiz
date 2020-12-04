d984b021fe37e4392d4d7c46e31c43b77c444d1b
turtles <me@ramon.nyc>
2020-12-04 08:26:00 -0800

    added functions to call the questions instead of running a loop. Moved global var up and added pseudo code of where start timer should be.

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-    // Start quiz from a welcome position and activates quiz with a START QUIZ btn.
-    document.getElementById("startBtn").addEventListener("click", function() {
-      document.getElementById("start").hidden = true;
-      document.getElementById("quiz").hidden = false;
-    }, false);
-
-    //Starts timer when startBtn is clicked.
-
-    //Start quiz with a score of 0.
-    var score = 0;
-
   // The array of questions for our quiz.
   var questions = [
     { q: "Question 1", c: ["1. Option", "2. Option", "3. Option", "4. Option"], a: 1 },
@@ -16,26 +5,26 @@
     { q: "Question 3", c: ["1. Option", "2. Option", "3. Option", "4. Option"], a: 3 },
     { q: "Question 4", c: ["1. Option", "2. Option", "3. Option", "4. Option"], a: 4 },
   ];
+  
+  
+  // Start quiz from a welcome position and activates quiz with a START QUIZ btn.
+  document.getElementById("startBtn").addEventListener("click", function() {
+    document.getElementById("start").hidden = true;
+    document.getElementById("quiz").hidden = false;
 
-  //Changes Question and Answer Text in quiz div using questions object.
-  document.getElementById("questionText").innerHTML = questions[0].q;
-  document.getElementById("c1Btn").innerHTML = questions[0].c[0];
-  document.getElementById("c2Btn").innerHTML = questions[0].c[1];
-  document.getElementById("c3Btn").innerHTML = questions[0].c[2];
-  document.getElementById("c4Btn").innerHTML = questions[0].c[3];
+    //Start quiz with a score of 0.
+    var score = 0;
 
-    //Set current answer to question.
-    var a = questions[0].a;
+    //Starts timer when startBtn is clicked.
 
-    //Listen for a button to be clicked to run feedback.
-    document.getElementById("c1Btn").addEventListener("click", clicked1);
-    document.getElementById("c2Btn").addEventListener("click", clicked2);
-    document.getElementById("c3Btn").addEventListener("click", clicked3);
-    document.getElementById("c4Btn").addEventListener("click", clicked4);
+    // First question
+    nextQuestion(3);
 
+  }, false);
+
+  function nextQuestion(i) {
     //Set and check answer.
-    function clicked1(){
-      var c = 1;
+    function clicked(c){
       if (c==a){
         document.getElementById("result").innerHTML = "Correct!";
       } else {
@@ -44,36 +33,22 @@
       document.getElementById("feedbackPanel").hidden = false;
     };
 
-    function clicked2(){
-      var c = 2;
-      if (c==a){
-        document.getElementById("result").innerHTML = "Correct!";
-      } else {
-        document.getElementById("result").innerHTML = "Wrong!";
-      }
-      document.getElementById("feedbackPanel").hidden = false;
-    };
+      //Changes Question and Answer Text in quiz div using questions object.
+      document.getElementById("questionText").innerHTML = questions[i].q;
+      document.getElementById("c1Btn").innerHTML = questions[i].c[0];
+      document.getElementById("c2Btn").innerHTML = questions[i].c[1];
+      document.getElementById("c3Btn").innerHTML = questions[i].c[2];
+      document.getElementById("c4Btn").innerHTML = questions[i].c[3];
 
-    function clicked3(){
-      var c = 3;
-      if (c==a){
-        document.getElementById("result").innerHTML = "Correct!";
-      } else {
-        document.getElementById("result").innerHTML = "Wrong!";
-      }
-      document.getElementById("feedbackPanel").hidden = false;
-    };
+      //Set current answer to question.
+      var a = questions[i].a;
 
-    function clicked4(){
-      var c = 4;
-      if (c==a){
-        document.getElementById("result").innerHTML = "Correct!";
-      } else {
-        document.getElementById("result").innerHTML = "Wrong!";
-      }
-      document.getElementById("feedbackPanel").hidden = false;
-    };
+      //Listen for a button to be clicked to run feedback.
+      document.getElementById("c1Btn").addEventListener("click", function(){ clicked(1)});
+      document.getElementById("c2Btn").addEventListener("click", function(){ clicked(2)});
+      document.getElementById("c3Btn").addEventListener("click", function(){ clicked(3)});
+      document.getElementById("c4Btn").addEventListener("click", function(){ clicked(4)});
+  }
 
-    //        document.getElementById("result").innerHTML = "Correct!";
-    //        document.getElementById("result").innerHTML = "Wrong!";
-    //        document.getElementById("feedbackPanel").hidden = false;
+      
+
