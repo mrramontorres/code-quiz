22474b304b52ad15e400588ceb04bafa57e1f2c3
turtles <me@ramon.nyc>
2020-12-03 21:22:50 -0800

    Got the start quiz button to change to quiz panel.

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
     // Psuedo Code Draft
-    // Need quiz to start from welcome position.
-  document.getElementById("okButton")
-    .addEventListener("click", function() {
-  document.getElementById("welcome").hidden = true;
-  document.getElementById("awesome").hidden = false;
-  }, false);
+
+    // Need quiz to start from a welcome position.
+    document.getElementById("startBtn").addEventListener("click", function() {
+      document.getElementById("start").hidden = true;
+      document.getElementById("quiz").hidden = false;
+    }, false);
+    
     // Need Bottom Btn to change start a timer, change question, hid text, replace text in Text Holder
     // Need answer to be recognized as correct or incorrect with feedback footer changed 
     // Need score adjusted and new question and answers to follow
     // Need either time or score to end quiz 
 
-
-
+/*
 
     // The array of questions for our quiz game.
     var questions = [
-      { q: "Question 1.", a: "a" },
-      { q: "Question 2.", a: "b" },
-      { q: "Question 3.", a: "c" },
-      { q: "Question 4.", a: "d" },
+      { q: "Question 1.", a: "1" },
+      { q: "Question 2.", a: "2" },
+      { q: "Question 3.", a: "3" },
+      { q: "Question 4.", a: "4" },
     ];
 
     // We start the game with a score of 0.
@@ -46,3 +46,5 @@
 
     // Add event listener to start quiz.
     startBtn.addEventListener("click", writePassword);
+
+*/
