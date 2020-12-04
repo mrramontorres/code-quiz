7b32f0f60ca8e44a61d7e731b704095c7f91c574
turtles <me@ramon.nyc>
2020-12-04 01:06:23 -0800

    added logic behind validated if user had selected correct or wrong answer.

--- script.js
+++ script.js
@@ -9,63 +9,71 @@
     //Start quiz with a score of 0.
     var score = 0;
 
-    // The array of questions for our quiz.
-    var questions = [
-      { q: "Question 1", c: ["1", "2", "3", "4"], a: "1" },
-      { q: "Question 2", c: ["1", "2", "3", "4"], a: "2" },
-      { q: "Question 3", c: ["1", "2", "3", "4"], a: "3" },
-      { q: "Question 4", c: ["1", "2", "3", "4"], a: "4" },
-    ];
+  // The array of questions for our quiz.
+  var questions = [
+    { q: "Question 1", c: ["1. Option", "2. Option", "3. Option", "4. Option"], a: 1 },
+    { q: "Question 2", c: ["1. Option", "2. Option", "3. Option", "4. Option"], a: 2 },
+    { q: "Question 3", c: ["1. Option", "2. Option", "3. Option", "4. Option"], a: 3 },
+    { q: "Question 4", c: ["1. Option", "2. Option", "3. Option", "4. Option"], a: 4 },
+  ];
 
-        //Changes Question and Answer Text in quiz div using questions object.
-        document.getElementById("questionText").innerHTML = questions[0].q;
-        document.getElementById("c1Btn").innerHTML = questions[0].c[0];
-        document.getElementById("c2Btn").innerHTML = questions[0].c[1];
-        document.getElementById("c3Btn").innerHTML = questions[0].c[2];
-        document.getElementById("c4Btn").innerHTML = questions[0].c[3];
+  //Changes Question and Answer Text in quiz div using questions object.
+  document.getElementById("questionText").innerHTML = questions[0].q;
+  document.getElementById("c1Btn").innerHTML = questions[0].c[0];
+  document.getElementById("c2Btn").innerHTML = questions[0].c[1];
+  document.getElementById("c3Btn").innerHTML = questions[0].c[2];
+  document.getElementById("c4Btn").innerHTML = questions[0].c[3];
 
-        //Set current answer to question.
-        var a = questions[0].a;
+    //Set current answer to question.
+    var a = questions[0].a;
 
-        //Listen for a button to be clicked to run feedback.
-        document.getElementById("c1Btn").addEventListener("click", click);
-        document.getElementById("c2Btn").addEventListener("click", click);
-        document.getElementById("c3Btn").addEventListener("click", click);
-        document.getElementById("c4Btn").addEventListener("click", click);
+    //Listen for a button to be clicked to run feedback.
+    document.getElementById("c1Btn").addEventListener("click", clicked1);
+    document.getElementById("c2Btn").addEventListener("click", clicked2);
+    document.getElementById("c3Btn").addEventListener("click", clicked3);
+    document.getElementById("c4Btn").addEventListener("click", clicked4);
 
-        //Change score based on answer and provide feedback.
-        function click(event) {
-          console.log("a button choice was clicked");
-          document.getElementById("feedback").hidden = false;
-        }
-
-    // Need Bottom Btn to change start a timer, change question, hid text, replace text in Text Holder
-    // Need answer to be recognized as correct or incorrect with feedback footer changed 
-    // Need score adjusted and new question and answers to follow
-    // Need either time or score to end quiz 
-
-/*
-    // Loop over every question object
-    for (var i = 0; i < questions.length; i++) {
-      // Display current question to user and ask OK/Cancel
-      var answer = confirm(questions[i].q);
-
-      // Compare answers
-      if ((answer === true && questions[i].a === "t") ||
-        (answer === false && questions[i].a === "f")) {
-        // Increase score
-        score++;
-        alert("Correct!");
+    //Set and check answer.
+    function clicked1(){
+      var c = 1;
+      if (c==a){
+        document.getElementById("result").innerHTML = "Correct!";
+      } else {
+        document.getElementById("result").innerHTML = "Wrong!";
       }
-      else {
-        alert("Wrong!");
+      document.getElementById("feedbackPanel").hidden = false;
+    };
+
+    function clicked2(){
+      var c = 2;
+      if (c==a){
+        document.getElementById("result").innerHTML = "Correct!";
+      } else {
+        document.getElementById("result").innerHTML = "Wrong!";
       }
-    }
+      document.getElementById("feedbackPanel").hidden = false;
+    };
 
-    // Show total at end
-    alert("You got " + score + "/" + questions.length);
+    function clicked3(){
+      var c = 3;
+      if (c==a){
+        document.getElementById("result").innerHTML = "Correct!";
+      } else {
+        document.getElementById("result").innerHTML = "Wrong!";
+      }
+      document.getElementById("feedbackPanel").hidden = false;
+    };
 
-    // Add event listener to start quiz.
-    startBtn.addEventListener("click", writePassword);
+    function clicked4(){
+      var c = 4;
+      if (c==a){
+        document.getElementById("result").innerHTML = "Correct!";
+      } else {
+        document.getElementById("result").innerHTML = "Wrong!";
+      }
+      document.getElementById("feedbackPanel").hidden = false;
+    };
 
-*/
+    //        document.getElementById("result").innerHTML = "Correct!";
+    //        document.getElementById("result").innerHTML = "Wrong!";
+    //        document.getElementById("feedbackPanel").hidden = false;
